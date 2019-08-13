4739631d551113404f3980212d17ad95965fddcc
Paulo Sergio Sabino de Araujo <paulo.sabino@tjdft.jus.br>
2019-08-13 13:34:44 -0300

    com rotas

--- tindev/Readme.md
+++ tindev/Readme.md
@@ -1,13 +1,26 @@
 ### Comandos
-- react-native init tindev - criação
-
-Emulador Android
 - https://docs.rocketseat.dev
+- react-native init tindev - criação
+- sudo apt install adb
+- adb devices
+- sudo yarn global add react-native-cli
+- react-native start - sobe o servidor
+- react-native run-android - envia o apk para o celular
+- yarn add react-navigation react-native-gesture-handler react-native-reanimated
+   - react-native-gesture-handler
+        #### ANDROID
+        - https://kmagiera.github.io/react-native-gesture-handler/docs/getting-started.html
+        - update MainActivity.java
+        - yarn react-native run-android
+        #### IOS
+        - https://kmagiera.github.io/react-native-gesture-handler/docs/getting-started.html
+        - No MAC instalar cocoapods
+        - na pasta do app: /tindev/ios
+            - pod install
+        - yarn react-native run-ios
+
+### Emulador Android
 - https://docs.rocketseat.dev/ambiente-react-native/android/emulador
-- via USB
-    - https://docs.rocketseat.dev/ambiente-react-native/usb/android
-        - sudo apt install adb
-        - adb devices
-        - sudo yarn global add react-native-cli
-        - react-native start - sobe o servidor
-        - react-native run-android - envia o apk para o celular
+
+### USB
+- https://docs.rocketseat.dev/ambiente-react-native/usb/android
--- tindev/src/pages/Login.js
+++ tindev/src/pages/Login.js
@@ -1,19 +1,64 @@
 import React from 'react'
-import {View, StyleSheet, Image,Text} from 'react-native'
+import {View, StyleSheet, Image,TextInput, TouchableOpacity, Text} from 'react-native'
 
 import logo from '../assets/logo.png'
 
-export default function Login(){
+export default function Login({ navigation}){
+    function handleLogin(){
+        navigation.navigate('Main')
+    }
+
     return (
     < View style={styles.container} >
-        <Text>teste</Text>
         <Image source={logo}/>
+        <TextInput 
+            //     enabled={platform.OS == 'ios'}
+            autoCapitalize='none'
+            autoCorrect={false}
+            placeholder="Digite seu usuário do Github" 
+            placeholderTextColor='#999'
+            style={styles.input}
+            />
+
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+            <Text style={styles.buttonText}>Enviar</Text>
+        </TouchableOpacity>
     </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-
+        flex:1,
+        backgroundColor: "#f5f5f5",
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 30
     },  
+    input:{
+        height: 46,
+        alignSelf: "stretch",
+        backgroundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: '#ddd',
+        borderRadius: 4,
+        marginTop: 20,
+        paddingHorizontal: 15,
+    },
+
+    button:{
+        height: 46,
+        alignSelf: 'stretch',
+        backgroundColor: '#DF4723',
+        borderRadius: 4,
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    buttonText:{
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+    }
 })
